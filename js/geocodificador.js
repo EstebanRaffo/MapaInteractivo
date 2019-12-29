4db07b3c38b8aed1b8c54126a7e5f355fd7a3160
unknown <e.fraffo@gmail.com>
2019-12-29 18:27:03 -0300

    29/12

--- js/geocodificador.js
+++ js/geocodificador.js
@@ -1,18 +1,18 @@
 geocodificadorModulo = (function () {
   var geocodificador // Geocodificador que dada una dirección devuelve una coordenada
   
-    // Permite obtener las coordenadas y las usa con la función llamada por parámtero
+    // Permite obtener las coordenadas y las usa con la función llamada por parámetro
   function usaDireccion (direccion, funcionALlamar) {
-        /* Completar la función usaDireccion(dirección,funcionALlamar)
+      /* Completar la función usaDireccion(dirección,funcionALlamar)
      para que se obtengan las coordenadas a partir de la dirección pasada por parámetro
      y que llame a la función pasada por parámetro con los siguientes parámetros
      dirección: la dirección pasada por parámetro
      coordenada: la ubicación de tipo google.maps.LatLng */
   }
 
-    // Inicializo el geocoder que obtiene las corrdenadas a partir de una dirección
+    // Inicializo el geocoder que obtiene las coordenadas a partir de una dirección
     // La variable dirección es igual al texto ingresado por el usuario
-    // Llama a la función usaDirecciin para agregarla a los listados y mostrarlo en el mapa
+    // Llama a la función usaDireccion para agregarla a los listados y mostrarlo en el mapa
   function inicializar () {
     var that = this
     geocodificador = new google.maps.Geocoder()
