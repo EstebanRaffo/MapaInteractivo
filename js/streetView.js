571aa2b685fce21a9161b68d04ca8ee961822da8
unknown <e.fraffo@gmail.com>
2020-01-04 18:55:53 -0300

    4/01

--- js/streetView.js
+++ js/streetView.js
@@ -2,9 +2,25 @@ streetViewModulo = (function () {
   var panorama // 'Visor' de StreetView
 
   function inicializar () {
-        /* Completar la función inicializar()  que crea un panorama
+        /* Completar la función inicializar() que crea un panorama
         en una posición y lo muestra en la página. */
+        var posicion = {lat: -34.6037389, lng: -58.3815704};
 
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: posicion,
+          zoom: 14
+        });
+
+        panorama = new google.maps.StreetViewPanorama(
+          document.getElementById('pano'), {
+            position: posicion,
+            pov: {
+              heading: 34,
+              pitch: 10
+            }
+        });
+        
+        map.setStreetView(panorama);
   }
 
     // Actualiza la ubicación del Panorama
